01d09a3427747ce16d90d6498154c77a3cb95a77
Kaushik kundu <kaushikkundu774@gmail.com>
2025-03-31 14:27:57 +0530

    first commit

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import {WebSocketServer, WebSocket} from "ws"
+
+const wss = new WebSocketServer({port:8080});
+
+let userCount = 0;
+let allSockets:WebSocket[] = [];
+
+wss.on("connection", (socket) => {
+    allSockets.push(socket);
+    userCount++;
+    console.log("user connected: " + userCount);
+    socket.on("message", (message) => {
+        console.log("message:" + message.toString());        
+        socket.send(message.toString() + " :sent from server")
+        for (let i = 0; i < allSockets.length; i++) {
+            const s = allSockets[i];
+            s.send(message.toString() + ": sent from server");
+            
+        }
+        
+    })
+    socket.on("disconnect", () => {{
+        allSockets = allSockets.filter((s) => s != socket);
+        userCount--;
+        console.log("user disconnected: " + userCount);
+    }}
+})
