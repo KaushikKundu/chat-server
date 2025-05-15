891ba4fc010c6b4cbf860f86b5db3a0ac7014508
Kaushik kundu <kaushikkundu774@gmail.com>
2025-05-15 13:02:44 +0530

    ws server naive implementation

--- dist/index.js
+++ dist/index.js
@@ -3,17 +3,34 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const ws_1 = require("ws");
 const wss = new ws_1.WebSocketServer({ port: 8080 });
 let userCount = 0;
-let allSockets = [];
+let allSockets = []; // [{roomId:1,socket:ws},{roomId;23,socket:ws},...]
 wss.on("connection", (socket) => {
-    allSockets.push(socket);
     userCount++;
     console.log("user connected: " + userCount);
     socket.on("message", (message) => {
-        console.log("message:" + message.toString());
-        socket.send(message.toString() + " :sent from server");
-        for (let i = 0; i < allSockets.length; i++) {
-            const s = allSockets[i];
-            s.send(message.toString() + ": sent from server");
+        var _a;
+        const parsedMessage = JSON.parse(message.toString());
+        try {
+            if (parsedMessage.type == "join") {
+                const { roomId } = parsedMessage.payload;
+                allSockets.push({
+                    roomId,
+                    socket
+                });
+                console.log("user joined room: " + roomId);
+            }
+            if (parsedMessage.type == "chat") {
+                // find current user roomID
+                const currentUserRoomId = (_a = allSockets.find(x => x.socket === socket)) === null || _a === void 0 ? void 0 : _a.roomId;
+                allSockets.forEach(user => {
+                    if (user.roomId === currentUserRoomId) {
+                        user.socket.send(parsedMessage.payload.message);
+                    }
+                });
+            }
+        }
+        catch (err) {
+            console.log(err);
         }
     });
 });
--- src/index.ts
+++ src/index.ts
@@ -8,26 +8,36 @@ interface User {
 }
 
 let userCount = 0;
-let allSockets:User[] = [];
+let allSockets:User[] = []; // [{roomId:1,socket:ws},{roomId;23,socket:ws},...]
 
 wss.on("connection", (socket) => {
     userCount++;
     console.log("user connected: " + userCount);
     socket.on("message", (message) => {
         const parsedMessage = JSON.parse(message.toString());
-        if(parsedMessage.type == "join") {
-            const {roomId} = parsedMessage.payload;
-            allSockets.push({
-                roomId,
-                socket
-            })
-            console.log("user joined room: " + roomId);
-        }
+        try{
 
-        if(parsedMessage.type == "chat") {
-            const {roomId} = parsedMessage.payload;
+            if(parsedMessage.type == "join") {
+                const {roomId} = parsedMessage.payload;
+                allSockets.push({
+                    roomId,
+                    socket
+                })
+                console.log("user joined room: " + roomId);
+            }
             
-            console.log("user joined room: " + roomId);
+            if(parsedMessage.type == "chat") {
+                // find current user roomID
+                const currentUserRoomId = allSockets.find(x => x.socket === socket)?.roomId;
+                allSockets.forEach(user  => {
+                    if(user.roomId === currentUserRoomId){
+                        user.socket.send(parsedMessage.payload.message);
+                    }
+                })
+                
+            }
+        }catch(err){
+            console.log(err);
         }
         
     })
